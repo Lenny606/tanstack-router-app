a3a9ee4efeebfd3f0d7e5cdf9f9990c30bfdecc3
Tomas Kravcik <kravcik@procorp.cz>
2025-03-01 11:41:50 +0100

    remove starter code

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="App-link"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
+    <div className="">
+    
     </div>
   );
 }
